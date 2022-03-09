70fbcdb5b62ae38f36c6da6587e7809024503b63
Clark Alesna <clark_alesna@hotmail.com>
2022-03-09 23:21:10 +0800

    initial commit

---
+++ src/Bifrost.ts
@@ -0,0 +1,137 @@
+export type BifrostWalletMetadata = {
+  apiVersion: string;
+  icon: string;
+  name: string;
+  id: string;
+}
+
+export type BifrostPaginate = {
+  page: number,
+  limit: number,
+};
+
+export type BifrostWalletApi = {
+  getUsedAddresses(paginate?: BifrostPaginate | undefined): Promise<string[]>;
+  getUtxos(value?: string | undefined): Promise<string[] | undefined>;
+  getCollateral(): Promise<string[] | undefined>;
+  experimental: {
+    getCollateral(): Promise<string[] | undefined>
+  };
+  signTx(tx: string, isPartialSign?: boolean): Promise<string>;
+  submitTx(tx: string): Promise<string>;
+  getRewardAddress(): Promise<string>;
+  getRewardAddresses(): Promise<string[]>;
+  signData(rewardAddrCborHex: string, message: string): Promise<string>;
+}
+
+export type BifrostWalletId = string | "nami" | "ccvault" | "flint";
+
+export class Bifrost {
+  static _cardano = (window as any).cardano;
+  static _api?: BifrostWalletApi = undefined;
+
+  public static getWallets(): BifrostWalletMetadata[] {
+    const result: BifrostWalletMetadata[] = [];
+    for (const i in Bifrost._cardano) {
+      const cardano = Bifrost._cardano;
+      const p = cardano[i];
+      if (p.apiVersion !== null && p.icon != null && p.name !== null) {
+        result.push({
+          apiVersion: p.apiVersion,
+          icon: p.icon,
+          name: p.name,
+          id: i.toString()
+        });
+      }
+    }
+    return result;
+  }
+
+  public static async enableAsync(id: BifrostWalletId): Promise<boolean> {
+    try {
+      const result = await Bifrost._cardano[id].enable();
+      if (typeof result === "boolean") Bifrost._api = Bifrost._cardano;
+      else Bifrost._api = result;
+      return true;
+    } catch (ex) {
+      console.log(ex);
+      return false;
+    }
+  }
+
+  public static async isEnabledAsync(id: BifrostWalletId) {
+    return await id === "nami" ? Bifrost._cardano.isEnabled() : Bifrost._cardano[id].isEnabled();
+  }
+
+  public static async setWalletAsync(id: BifrostWalletId) {
+    if (await this.isEnabledAsync(id)) {
+      await Bifrost.enableAsync(id);
+    } else Bifrost._throwNoAPIError();
+  }
+
+  public static async signTxRawAsync(txCborHex: string, isPartialSign?: boolean): Promise<string> {
+    if (Bifrost._api !== undefined)
+      return await Bifrost._api.signTx(txCborHex, isPartialSign);
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async submitTxRawAsync(txCborHex: string): Promise<string> {
+    if (Bifrost._api !== undefined)
+      return await Bifrost._api.submitTx(txCborHex);
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async getUsedAddressesRawAsync(): Promise<string[]> {
+    if (Bifrost._api !== undefined)
+      return await Bifrost._api.getUsedAddresses();
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async getUtxosRawAsync(valueCborHex?: string): Promise<string[]> {
+    if (Bifrost._api !== undefined)
+      return await Bifrost._api.getUtxos(valueCborHex);
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async getCollateralRawAsync(): Promise<string[]> {
+    if (Bifrost._api !== undefined)
+      return Bifrost._api.getCollateral ? Bifrost._api.getCollateral() : await Bifrost._api.experimental.getCollateral();
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async getRewardAddressAsync(): Promise<string | undefined> {
+    if (Bifrost._api !== undefined) {
+      return await Bifrost._api.getRewardAddress();
+    }
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async getRewardAddressesAsync(): Promise<string[] | undefined> {
+    if (Bifrost._api !== undefined) {
+      return await Bifrost._api.getRewardAddresses();
+    }
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  public static async signDataRawAsync(rewardAddrCborHex: string, message: string, walletId: BifrostWalletId) {
+    if (Bifrost._api !== undefined) {
+      if (walletId === "nami")
+        return await Bifrost._cardano.signData(rewardAddrCborHex, message);
+      else
+        return await Bifrost._api.signData(rewardAddrCborHex, message);
+    }
+    else
+      Bifrost._throwNoAPIError();
+  }
+
+  private static _throwNoAPIError() {
+    throw Error('No API available, is the wallet connection enabled?');
+  }
+}
